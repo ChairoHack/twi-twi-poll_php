0a0fbbff7c5c22065ea1f5fc1b209cbe2f93827b
KazusaTsubota <chachairo.chairo@gmail.com>
2021-10-05 22:54:40 +0900

    heroku prepare

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'f9b1bc144b953203a3a9659ced929538379bbf42',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'f9b1bc144b953203a3a9659ced929538379bbf42',
+            'dev_requirement' => false,
+        ),
+    ),
+);
